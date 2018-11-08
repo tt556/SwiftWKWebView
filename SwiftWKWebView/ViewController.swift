c94a98a86108c2ea8612f78a3044a5cc2de912bb
tt556 <so888tk@gmail.com>
2018-11-08 12:33:08 +0900

    Initial Commit

---
+++ SwiftWKWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftWKWebView
+//
+//  Created by 神崎泰旗 on 2018/11/08.
+//  Copyright © 2018年 taiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
